f63fcef4f8bc6ec80f59bf26102f6c0101f40421
mi8976 <163011439+mi8976@users.noreply.github.com>
2024-08-07 10:01:33 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,15 @@
-# isso serve para mostrar um comentário
 
-'''
-kk
-kk
-kkk
-'''
-
-print  ("seja bem vindo")
 
 nome = imput (" Escreva seu nome")
-print ("bem-vindo,", nome + "!")
+print("bem-vindo,", nome + "!")
+
+print("Como posso te ajudar hoje?")
+
+print("1 - exames")
+print("consulta")
+print("prescrição médica")
+
+
+opcao = int(input)("digite o número desejado")
+if opcao == 1:
+    print("")
