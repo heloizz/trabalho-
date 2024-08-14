f72c0079c25500a54bfd3d05a5bdf9368b580764
mi8976 <163011439+mi8976@users.noreply.github.com>
2024-08-14 09:28:28 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,10 +6,14 @@ print("bem-vindo,", nome + "!")
 print("Como posso te ajudar hoje?")
 
 print("1 - exames")
-print("consulta")
-print("prescrição médica")
+print("2 - consulta")
+print("3 - prescrição médica")
 
 
 opcao = int(input)("digite o número desejado")
 if opcao == 1:
-    print("")
+    print("digite o seu cpf")
+if opcao == 2:
+    print("verificar datas diponíveis")
+if opcao == 3:
+    print("digite o seu cpf")
