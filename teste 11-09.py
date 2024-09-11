23c8c541212ebc337cedbc2fe4c54fab4c6df518
mi8976 <163011439+mi8976@users.noreply.github.com>
2024-09-11 11:09:22 -0300

    Add files via upload

---
+++ teste 11-09.py
@@ -0,0 +1,12 @@
+#aprendendo a fazer um loop
+
+#iniciando a variavel 
+
+contador=0
+
+#utilizando while
+
+while contador < 50:
+    print("o valor do contador é:",contador)
+    contador += 1 
+    
